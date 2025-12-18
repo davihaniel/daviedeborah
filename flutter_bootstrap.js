4e8c146c3706b69aac630531a92592f7cd934719
davihaniel <davihaniel@users.noreply.github.com>
2025-12-18 21:15:22 +0000

    deploy: 97b2a983ef7e1ad0c65422aba65332b46be46122

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3260911343"
+    serviceWorkerVersion: "244531864"
   }
 });
